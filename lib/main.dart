58605845539491cbefc27eeb6e51a247598d27c2
Mohamed A <gottlicher.stern@gmail.com>
2019-06-25 15:05:41 +0200

    Definitely the final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:redux/redux.dart';
 import 'package:weatheria/app.dart';
 import 'package:weatheria/redux/appstate.dart';
@@ -8,6 +9,6 @@ import 'package:redux_logging/redux_logging.dart';
 
 void main() {
   final store = new Store<AppState>(appStateReducer, initialState: AppState.initialState(), middleware: [appStateMiddleWare(), LoggingMiddleware.printer()]);
-
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(Weatheria(store: store));
 } 
